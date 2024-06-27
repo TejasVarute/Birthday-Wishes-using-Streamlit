8b42a34062f995a69b6712c779f2045b2848b796
Tejas Varute <65999219+TejasVarute@users.noreply.github.com>
2024-06-27 23:04:11 +0530

    Update Happy_birthday_streamlit.py

--- Happy_birthday_streamlit.py
+++ Happy_birthday_streamlit.py
@@ -5,12 +5,11 @@ def wish(name):
     st.set_page_config(page_title="Happy Birthday Wisher", layout="centered")
     if name:
         img = "./data/img.jpg"
-        st.audio("./data/song.mp3", format="audio/mp3", start_time=0, autoplay=True, loop=True)
+        st.audio("./data/song.mp3", format="audio/mp3", start_time=0, autoplay=1, loop=1)
         aud_style = """
                     <style>
                     audio{
                         display: none;
-                        controls: autoplay;
                         }
                     </style>
                     """

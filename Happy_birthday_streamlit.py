64f72e8f35a243c9f346070e9c173aeae23e7ce2
Tejas Varute <65999219+TejasVarute@users.noreply.github.com>
2024-06-27 22:51:28 +0530

    Update Happy_birthday_streamlit.py

--- Happy_birthday_streamlit.py
+++ Happy_birthday_streamlit.py
@@ -20,7 +20,7 @@ def wish(name):
         image_style = '''
             <style>
                 h1 {
-                    height: 70px;
+                    #height: 70px;
                     font-size: 3rem;
                     background: linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722);
                     -webkit-background-clip: text;

e6400cf635815c6d0de586ec0c0e45a5594254c5
Tejas Varute <65999219+TejasVarute@users.noreply.github.com>
2024-06-27 22:47:54 +0530

    Update Happy_birthday_streamlit.py

--- Happy_birthday_streamlit.py
+++ Happy_birthday_streamlit.py
@@ -2,15 +2,17 @@ import streamlit as st
 import time
 
 def wish(name):
-    st.set_page_config(page_title="Happy Birthday Wisher - By Tejas Varute", layout="centered")
-
+    st.set_page_config(page_title="Happy Birthday Wisher -By Tejas Varute", layout="centered")
+    
+    st.audio("./data/song.mp3", format="audio/wav", start_time=0, autoplay=True, loop=True)
+    aud_style="<style> audio{ display: none; } </style>"
+    st.html(aud_style)
+    
     if name:
-        img = ".data/img.jpg"
+        img = "./data/img.jpg"
 
-        #aud = "./data/song.mp3"
-        #st.audio(aud, format="audio/wav", start_time=0, autoplay=True, loop=True)
-        
-        _,col2,_ = st.columns(3)
+        _,col2,_ = st.columns(3)
+
         with col2:
             st.image(img, width=250)
 
@@ -21,6 +23,7 @@ def wish(name):
         image_style = '''
             <style>
                 h1 {
+                    height: 70px;
                     font-size: 3rem;
                     background: linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722);
                     -webkit-background-clip: text;
@@ -29,14 +32,10 @@ def wish(name):
                 img{
                     border-radius: 20%;
                 }
-                audio{
-                    display: none;
-                }
             </style>
             '''
         st.html(image_style)
         while(True):
             time.sleep(1.7)
             st.balloons()
-
 wish("SHRIPAD")
